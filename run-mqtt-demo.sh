d05ba0a5658062b183016a25a289443722854a77
Kaj-Michael Lang <milang@tal.org>
2014-11-03 21:41:57 +0200

    Use variable for host

--- run-mqtt-demo.sh
+++ run-mqtt-demo.sh
@@ -3,14 +3,16 @@
 S=60
 N=120
 
+HOST=192.168.1.249
+
 while [ 1 ]; do
- mosquitto_pub -i demo-published -t stop/1170 -r -m $S
- mosquitto_pub -i demo-published -t stop/1170/next -r -m $N
- sleep 1
+ mosquitto_pub -h ${HOST} -i talorg-publisher -t stop/1170 -r -m $S
+ mosquitto_pub -h ${HOST} -i talorg-publisher -t stop/1170/next -r -m $N
  let S=S-1
  let N=N-1
  if [ $S -eq 0 ]; then
   S=$N
   N=120
  fi
+ sleep 1
 done
